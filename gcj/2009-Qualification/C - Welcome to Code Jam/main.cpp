832e30f38fbd2162b2be9052e8e83cc64de153ee
Arkhaix <arkhaix@arkhaix.com>
2012-04-17 22:15:59 -0700

    added cstdio and cstring includes to template.cpp (for memset).  solved GCJ 2009 Qualification C.

--- gcj/2009-Qualification/C - Welcome to Code Jam/main.cpp
+++ gcj/2009-Qualification/C - Welcome to Code Jam/main.cpp
@@ -1,6 +1,8 @@
 #include <algorithm>
 #include <cctype>
 #include <cmath>
+#include <cstdio>
+#include <cstring>
 #include <functional>
 #include <iostream>
 #include <list>
@@ -19,6 +21,9 @@ typedef vector<int> VI; typedef vector<string> VS;
 typedef unsigned int uint;
 typedef unsigned long long ull;
 
+string target = "welcome to code jam";
+int ways[20][501];
+
 int main(void)
 {
 	string line;
@@ -31,7 +36,42 @@ int main(void)
 
 	for(uint testCase=0; testCase < numTests; testCase++)
 	{
-		cout << "Case #" << testCase+1 << ": " << endl;
+	    getline(cin, line);
+
+	    memset(ways, 0, sizeof(ways));
+
+	    for(uint i=0;i<line.length();i++)
+	    {
+            for(uint j=0;j<target.length();j++)
+            {
+                if(line[i] == target[j])
+                {
+                    //q welcome to code jam p
+                    if(j == 0)
+                    {
+                        ways[j][i]++;
+                        ways[j][i] %= 10000;
+                    }
+                    else
+                    {
+                        for(uint k=0;k<i;k++)
+                        {
+                            ways[j][i] += ways[j-1][k];
+                            ways[j][i] %= 10000;
+                        }
+                    }
+                }
+            }
+	    }
+
+        int res = 0;
+        for(uint i=0;i<line.length();i++)
+        {
+            res += ways[target.length()-1][i];
+            res %= 10000;
+        }
+
+        printf("Case #%d: %04d\n", testCase+1, res);
 	}
 
 	return 0;
--- gcj/template.cpp
+++ gcj/template.cpp
@@ -1,6 +1,8 @@
 #include <algorithm>
 #include <cctype>
 #include <cmath>
+#include <cstdio>
+#include <cstring>
 #include <functional>
 #include <iostream>
 #include <list>
